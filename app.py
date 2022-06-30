47062e7566b43858666c60b6f4b6b73d5cf8024c
Victor Sago <VictorSago01@gmail.com>
2022-06-30 15:29:51 +0200

    Implemened read_name function for the api

--- app.py
+++ app.py
@@ -2,27 +2,42 @@ from flask import *
 import datetime
 import sqlite3
 
+
+# TODO refactor + cleanup
+
 def read_phonelist(C):
     cur = C.cursor()
     cur.execute("SELECT * FROM phonelist;")
     rows = cur.fetchall()
     cur.close()
     return rows
+
 def read_phone(C, name):
     cur = C.cursor()
-    print(f"SELECT phone FROM phonelist WHERE name = '{name}';")
+    print(f"SELECT phone FROM phonelist WHERE name = '{name}';")            #TODO cleanup diagnostics
     cur.execute(f"SELECT phone FROM phonelist WHERE name = '{name}';")
     rows = cur.fetchall()
     cur.close()
     return rows
+
+def read_name(C, phone):
+    cur = C.cursor()
+    print(f"SELECT name FROM phonelist WHERE phone = '{phone}';")           #TODO cleanup diagnostics
+    cur.execute(f"SELECT name FROM phonelist WHERE phone = '{phone}';")
+    rows = cur.fetchall()
+    cur.close()
+    return rows
+
 def add_phone(C, name, phone):
     cur = C.cursor()
     cur.execute(f"INSERT INTO phonelist VALUES ('{name}', '{phone}');")
     cur.close()
+
 def delete_phone(C, name):
     cur = C.cursor()
     cur.execute(f"DELETE FROM phonelist WHERE name = '{name}';")
     cur.close()
+
 def save_phonelist(C):
     cur = C.cursor()
     try:
@@ -30,6 +45,7 @@ def save_phonelist(C):
     except:
         print("No changes!")
     cur.close()
+
 
 app = Flask(__name__)
 
@@ -67,6 +83,7 @@ def insert_func():
 def api_func():
     conn = sqlite3.connect("phone.db")
     args=request.args
+    print("Api_func: args:", args)                                          #TODO cleanup diagnostics
     action = args.get('action', default="Bad action", type=str)
     if action == "Bad action":
         return render_template('api_usage.html', action=action)
@@ -75,9 +92,19 @@ def api_func():
         if name == "No name":
             return render_template('api_usage.html', action=action)
         phone = read_phone(conn, name)
+        print("Api_func-phone:", phone)                                     #TODO cleanup diagnostics
         if len(phone) < 1:
-            return "not found"
+            return "Not Found"
         return phone[0][0]
+    elif action == 'name':
+        phone = args.get('phone', default="No phone", type=str)
+        if phone == "No phone":
+            return render_template('api_usage.html', action=action)
+        name = read_name(conn, phone)
+        print("Api_func-name:", name)                                       #TODO cleanup diagnostics
+        if len(name) < 1:
+            return "Not Found"
+        return name[0][0]
     else:
         return f"Unknown action: '{action}'"
 
